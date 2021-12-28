29dfc07ac8dc2ad4f87237878efee1fa6db07896
Elizabeth Teena <94455588+grace-teena@users.noreply.github.com>
2021-12-28 11:08:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "<h1><center>Hello, World! Flask Application</h1></center>"
+
+
+app.run(host='0.0.0.0', port=5000)
+
